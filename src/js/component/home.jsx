f10fd1f48bf0a1d1dcf5b9bb96e861c75ae7d9b4
GustavoColombia <157833803+GustavoColombia@users.noreply.github.com>
2024-05-27 17:37:21 +0000

    lista de tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  function addTask(e) {
+    if (e.key === "Enter") {
+      setTasks([...tasks, task])
+      setTask("")
+    }
+  }
+  /*
+  0-leer
+  1-cocinar
+  2-barrer 
+  */
+
+  function deleteTask(e, id) {
+    e.preventDefault() // esta funcion (preventDefault) detiene el envio de informacion del formulario
+    let aux = []
+    aux = tasks.filter((item, index) => {
+      if (index != id) {
+        return item
+      }
+    })
+    setTasks(aux)
+  }
+
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">Todos</h1>
+      <div className="card">
+        <div className="input-group">
+          <input value={task} onChange={(e) => setTask(e.target.value)} onKeyDown={(e) => addTask(e)} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></input>
+        </div>
+
+        <ul className="list-group list-group-flush">
+          {tasks.map((item, id) => (
+            <li key={id} className="list-group-item">
+              {item}
+              <button className="btn btn-outline-danger float-end" onClick={(e) => deleteTask(e, id)}><i className="fa fa-trash"></i></button>
+            </li>
+          ))}
+
+        </ul>
+
+      </div>
+    </div>
+  );
 };
 
 export default Home;
